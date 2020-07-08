c506708d1bdb80ec9a8b013163bb620a13beb2ed
Hexx King <hexxking13@gmail.com>
2020-07-07 18:01:47 -0700

    feature 2 complete

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
 $.ajax('data/page-1.json', { method: "GET", dataType: "JSON" })
-  .then(data => {
-    data.forEach(animal => {
-      new Animal(animal).creatorBuilder();  
-    })
-    renderDropDown();
+.then(data => {
+  data.forEach(animal => {
+    new Animal(animal).creatorBuilder();  
   })
+  renderDropDown();
+})
 
-  const keywordArr = [];
-  
+const keywordArr = [];
+
 function Animal(obj) {
   this.image = obj.image_url;
   this.title = obj.title;
@@ -20,17 +20,17 @@ function Animal(obj) {
 
 Animal.prototype.creatorBuilder = function () {
   const myTemplate = $('#photo-template').html();
-
-  const $newSection = $(`<section>${myTemplate}</section>`);
-
+  
+  const $newSection = $(`<section class=${this.keyword}>${myTemplate}</section>`);
+  
   $newSection.find('h2').text(this.title);
-
+  
   $newSection.find('p').text(this.description);
-
+  
   $newSection.find('img').attr('src', this.image);
-
+  
   $newSection.find('img').attr('alt', this.description);
-
+  
   $('main').append($newSection);
   
   if (keywordArr.includes(this.keyword) === false){
@@ -44,15 +44,20 @@ Animal.prototype.creatorBuilder = function () {
 
 // Create a <select> element which contains unique <option> elements extracted dynamically from the JSON file, one for each keyword.
 function renderDropDown() {
-for (let i = 0; i < keywordArr.length; i++){
-let keyword = keywordArr[i];
-  $("select").append(`<option>${keyword}</option>`);
+  for (let i = 0; i < keywordArr.length; i++){
+    let keyword = keywordArr[i];
+    $("select").append(`<option>${keyword}</option>`);
+  }
 }
-}
 
-//Use an event handler to respond when the user chooses an option from the select menu. Hide all of the images, then show those whose keyword matches the option chosen.
-// $("select").on("click", "option", eventHandler)
-
-
-
+//Use an event handler to respond (click event) when the user chooses an option from the select menu
+//The code below was found by Meghan at https://api.jquery.com/click/
+// lots of guidance from TA Skyler
+$("select").on('change', function(){
+  console.log("handler for .click() called", $(this).val());
+  // Hide all of the images (:hidden?)
+  $('section').hide();
+  // then show those whose keyword matches the option chosen.  (:visible?) 
+  $(`section[class=${$(this).val()}]`).show();
+});
 
